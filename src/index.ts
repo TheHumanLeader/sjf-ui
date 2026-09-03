02030af32a4da8b893a05558f6636636c6a831de
TheHumanLeader <59379767+TheHumanLeader@users.noreply.github.com>
2026-09-03 13:48:51 +0800

    feat: export Form and Input

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import './styles/index.css'
 
+export { default as SjfForm } from './components/Form/index.vue'
+export * from './components/Form'
+
+export { default as SjfInput } from './components/Input/index.vue'
+export * from './components/Input'
+
 export { default as SjfLabel } from './components/Label/index.vue'
 export * from './components/Label'
+
 export * from './core/size'
 export * from './core/theme'
