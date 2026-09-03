b18d87b249b7cf12278618da8a8ca57171d7ddfc
TheHumanLeader <59379767+TheHumanLeader@users.noreply.github.com>
2026-09-03 14:43:38 +0800

    feat(input): define v0.1 public API

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,16 +1,21 @@
+import type { SjfSize } from '../../core/size'
 import type { SjfHorizontalAlign, SjfLabelOption } from '../Label'
 
 export type SjfInputModelValue = string | number | null | undefined
 
 export interface SjfInputProps {
   modelValue?: SjfInputModelValue
+
+  // Label / Form integration
   label?: string
   labelOption?: SjfLabelOption
   colSpan?: number
   rowSpan?: number
+  size?: SjfSize
   labelAlign?: SjfHorizontalAlign
   contentAlign?: SjfHorizontalAlign
 
+  // Native input essentials
   type?: string
   name?: string
   placeholder?: string
@@ -19,4 +24,18 @@ export interface SjfInputProps {
   disabled?: boolean
   readonly?: boolean
   error?: boolean
+  maxlength?: number
+  minlength?: number
+
+  // SJF interaction helpers
+  clearable?: boolean
+  showPassword?: boolean
+  showCount?: boolean
+}
+
+export interface SjfInputExpose {
+  focus: () => void
+  blur: () => void
+  select: () => void
+  input: HTMLInputElement | null
 }
