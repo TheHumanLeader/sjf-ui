9688b148cd54bd677259df603aeffe71e4d0992e
TheHumanLeader <59379767+TheHumanLeader@users.noreply.github.com>
2026-09-03 12:12:18 +0800

    docs(theme): apply stored theme before mount

--- docs/main.ts
+++ docs/main.ts
@@ -1,6 +1,9 @@
 import { createVaporApp } from 'vue'
 import App from './App.vue'
+import { applySjfTheme, readStoredSjfTheme } from '@/core/theme'
 import './styles.css'
+import './theme.css'
 import './component-demo.css'
 
+applySjfTheme(readStoredSjfTheme())
 createVaporApp(App).mount('#app')
